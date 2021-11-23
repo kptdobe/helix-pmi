d3ced2a9e8d57667c693a3ceee6fdf7f6fee6d43
Luca Nerlich <luca.nerlich@googlemail.com>
2021-11-23 14:30:45 +0100

    added social buttons - somewhat wip

--- blocks/social-wrapper/social-wrapper.js
+++ blocks/social-wrapper/social-wrapper.js
@@ -11,22 +11,29 @@ class SocialWrapper {
 
     //  Facebook
     const facebookButton = createTag('button', { class: 'social-wrapper_button' });
-    facebookButton.innerText = 'Facebook';
+    facebookButton.innerText = 'F';
+    facebookButton.setAttribute('data-service', 'facebook');
+    facebookButton.setAttribute('data-url', 'https://www.pmi.com/markets/italy/it/chi-siamo/nostri-prodotti?utm_source=share+button+facebook&utm_medium=social&utm_campaign=share');
     this.el.appendChild(facebookButton);
 
     //  Twitter
     const twitterButton = createTag('button', { class: 'social-wrapper_button' });
-    twitterButton.innerText = 'Twitter';
+    twitterButton.innerText = '🐤';
+    twitterButton.setAttribute('data-service', 'twitter');
+    twitterButton.setAttribute('data-url', 'https://www.pmi.com/markets/italy/it/chi-siamo/nostri-prodotti?utm_source=share+button+twitter&utm_medium=social&utm_campaign=share');
     this.el.appendChild(twitterButton);
 
     //  LinkedIn
     const linkedInButton = createTag('button', { class: 'social-wrapper_button' });
-    linkedInButton.innerText = 'LinkedIn';
+    linkedInButton.innerText = 'LI';
+    linkedInButton.setAttribute('data-service', 'linkedin');
+    linkedInButton.setAttribute('data-url', 'https://www.pmi.com/markets/italy/it/chi-siamo/nostri-prodotti?utm_source=share+button+linkedin&utm_medium=social&utm_campaign=share');
     this.el.appendChild(linkedInButton);
 
     //  E-Mail
-    const mailButton = createTag('button', { class: 'social-wrapper_button' });
-    mailButton.innerText = 'E-Mail';
+    const mailButton = createTag('a', { class: 'social-wrapper_button' });
+    mailButton.setAttribute('href', 'mailto:?subject=Take a look at this on PMI.com - I nostri prodotti&body=I found this on PMI.com and thought that it might interest you.%0D%0AI nostri prodotti %0D%0AI prodotti Philip Morris in Italia: da azienda leader nel commercio delle sigarette tradizionali alla trasformazione verso i prodotti senza fumo come IQOS.\t %0D%0ARead more: https://www.pmi.com/markets/italy/it/chi-siamo/nostri-prodotti%3Futm_source%3Dshare%2Bbutton%2Bemail%26utm_medium%3Demail%26utm_campaign%3Dshare');
+    mailButton.innerText = '✉';
     this.el.appendChild(mailButton);
   };
 }
