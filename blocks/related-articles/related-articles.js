7048cc74e160aa58d47c1dcd9beaa59b9b610e8e
Luca Nerlich <luca.nerlich@googlemail.com>
2021-11-23 10:30:56 +0100

    parse and set a tag with correct url

--- blocks/related-articles/related-articles.js
+++ blocks/related-articles/related-articles.js
@@ -1,23 +1,43 @@
 import createTag from '../gnav/gnav-utils.js';
-import { fetchDomain, fetchTextFromMarkup } from '../../scripts/utils.js';
+import { fetchDomain, fetchTextFromMarkup, getUrlForEnvironment } from '../../scripts/utils.js';
 
 class RelatedArticles {
-  constructor(parsedArticles, el) {
+  constructor(articleContainers, el) {
     this.el = el;
-    this.parsedArticles = parsedArticles;
+    this.articleContainers = articleContainers;
     this.desktop = window.matchMedia('(min-width: 900px)');
   }
 
   init = async () => {
     const wrapper = createTag('div', { class: 'related-articles_wrapper' });
 
-    for (let i = 0; i < this.parsedArticles.length; i += 1) {
+    // todo display category up top
+
+    for (let i = 0; i < this.articleContainers.length; i += 1) {
       // todo loop over parsed Articles and create a "related-articles_item" for each.
       const item = createTag('div', { class: 'related-articles_item' });
-      const parsedArticle = this.parsedArticles[i];
-      console.log('parsedArticle', parsedArticle);
+      const title = createTag('div', { class: 'related-articles_item-title' });
+      const parsedArticle = this.articleContainers[i].text;
+      const titleText = parsedArticle.querySelector('html body div h1').innerHTML;
+      title.innerHTML = titleText;
+      const subtitle = parsedArticle.querySelector('html body div p');
+      const imageWrapper = parsedArticle.querySelector('html body div p:nth-child(3) picture');
+      console.log('titleText', titleText);
+      console.log('subtitle', subtitle);
+      console.log('imageWrapper', imageWrapper);
+
+      const aTag = createTag('a', { class: 'related-articles_link' });
+      aTag.href = this.articleContainers[i].url;
+      aTag.title = titleText;
+      item.append(aTag);
+
+      aTag.append(imageWrapper);
+      aTag.append(title);
+      // item.append(title);
       wrapper.append(item);
     }
+
+    // todo wrap in a tag
 
     // remove 'old' block of related articles links
     this.el.firstChild.remove();
@@ -36,13 +56,19 @@ export default async function init(block) {
     if (articlesMarkupText) {
       try {
         const parser = new DOMParser();
-        const parsedArticles = [];
+        const articleContainers = [];
 
         for (let i = 0; i < articlesMarkupText.length; i += 1) {
-          parsedArticles.push(parser.parseFromString(articlesMarkupText[i], 'text/html'));
+          const transformedUrl = getUrlForEnvironment(relatedArticleUrls[i].querySelector('a')
+            .getAttribute('href'));
+          const articleContainer = {
+            text: parser.parseFromString(articlesMarkupText[i], 'text/html'),
+            url: transformedUrl,
+          };
+          articleContainers.push(articleContainer);
         }
 
-        const relatedArticles = new RelatedArticles(parsedArticles, block);
+        const relatedArticles = new RelatedArticles(articleContainers, block);
         await relatedArticles.init();
       } catch (error) {
         // eslint-disable-next-line no-console
