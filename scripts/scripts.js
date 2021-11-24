c2cfc66137920cde9a204492bdb891880e31089d
Alexandre Capt <acapt@adobe.com>
2021-11-24 11:49:32 +0100

    chore: remove class

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -507,9 +507,9 @@ async function loadEager(doc) {
   const main = doc.querySelector('main');
   if (main) {
     decorateMain(main);
-    doc.querySelector('body')
-      .classList
-      .add('appear');
+    // doc.querySelector('body')
+    //   .classList
+    //   .add('appear');
 
     const block = doc.querySelector('.block');
     const hasLCPBlock = (block && LCP_BLOCKS.includes(block.getAttribute('data-block-name')));
